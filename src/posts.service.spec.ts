d541d36e97b98ad82860777cc82d73b39c772c3e
zlocate <zlocated@gmail.com>
2022-10-04 18:18:33 +0300

    feat: add solution

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,15 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const createdPost = postsService.create(post);
+
+    expect(postsService.find(createdPost.id)).toEqual(createdPost);
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    const createdPost = postsService.create(post);
+    const foundPost = postsService.find(createdPost.id);
+
+    expect(foundPost).toMatchObject(post);
   });
 });
